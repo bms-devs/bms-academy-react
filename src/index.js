416ead069f5ede924cd308bc531574272181edc3
Kamil Rafałko <kamil.rafalko@bms.com.pl>
2017-08-16 08:39:43 +0200

    An Interactive Component

--- src/index.js
+++ src/index.js
@@ -3,18 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            value: null
+        }
+    }
+
     render() {
         return (
-            <button className="square">
-                {this.props.value}
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
-        );
+        )
     }
 }
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square value={i} />
+        return <Square />
     }
 
     render() {
