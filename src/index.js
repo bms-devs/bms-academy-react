61ea09ff0d730c6516e886f58bb2905851e52984
Kamil Rafałko <kamil.rafalko@bms.com.pl>
2017-08-16 10:25:07 +0200

    Taking Turns

--- src/index.js
+++ src/index.js
@@ -12,14 +12,18 @@ class Board extends React.Component {
     constructor() {
         super();
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            xIsNext: true
         }
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice()
-        squares[i] = 'X'
-        this.setState({squares: squares})
+        squares[i] = this.state.xIsNext ? 'X' : '0'
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        })
 
     }
 
@@ -31,7 +35,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = "Next player: X"
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         return (
             <div>
                 <div className="status">{status}</div>
