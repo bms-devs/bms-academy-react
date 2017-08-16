96e7bd87929fbaff4b09890f4424f458d207f41d
Kamil Rafałko <kamil.rafalko@bms.com.pl>
2017-08-16 09:55:48 +0200

    Declaring a Winner

--- src/index.js
+++ src/index.js
@@ -17,8 +17,31 @@ class Board extends React.Component {
         }
     }
 
+    calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ]
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a]
+            }
+        }
+        return null
+    }
+
     handleClick(i) {
         const squares = this.state.squares.slice()
+        if (this.calculateWinner(squares) || squares[i]) {
+            return
+        }
         squares[i] = this.state.xIsNext ? 'X' : '0'
         this.setState({
             squares: squares,
@@ -35,7 +58,8 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = this.calculateWinner(this.state.squares)
+        const status = winner ? 'Winner: ' + winner : 'Next player: ' + (this.state.xIsNext ? 'X' : '0')
         return (
             <div>
                 <div className="status">{status}</div>
